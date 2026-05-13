72e6fd07e6b57e34cdc2090d03d4c1f26f9e7c2d
Carlos Abarzua <ca.abarzuac@profesor.duoc.cl>
2026-05-13 15:01:38 -0400

    Nuevo .bat

--- build-all.bat
+++ build-all.bat
@@ -1,21 +1,18 @@
+rem $env:DOCKER_BUILDKIT=0
 rem docker rm -f $(docker ps -aq)
 FOR /f %%i IN ('docker ps -aq') DO docker rm -f %%i
+FOR /f %%i IN ('docker images -aq') DO docker rmi -f %%i
 cd api-gateway
-rmdir /s /q target
 call .\mvnw clean package -DskipTests
 
 cd ../auth-ser
-rmdir /s /q target
 call .\mvnw clean package -DskipTests
 
 cd ../cliente-service
-rmdir /s /q target
 call .\mvnw clean package -DskipTests
 
 cd ../compra-service
-rmdir /s /q target
 call  .\mvnw clean package -DskipTests
 
 cd ../producto-service
-rmdir /s /q target
 call .\mvnw clean package -DskipTests
